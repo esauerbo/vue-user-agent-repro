acf5d5e7b25facb95a048ee90be4ed94ef2eae64
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2023-10-25 14:07:56 -0400

    adding project files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "vueuseragent5857c6b6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
